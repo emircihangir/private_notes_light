849a3807c56f0cd7e3892ba353514a272364c121
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2026-02-06 15:43:02 +0300

    Add login screen

--- lib/presentation/login_screen.dart
+++ lib/presentation/login_screen.dart
@@ -1,1 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+class LoginScreen extends ConsumerStatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends ConsumerState<LoginScreen> {
+  String passwordInput = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: SafeArea(
+        child: Column(
+          mainAxisAlignment: .center,
+          spacing: 32,
+          children: [
+            Text('Welcome', style: Theme.of(context).textTheme.headlineLarge),
+            PasswordTextField(onChanged: (value) => passwordInput = value),
+            FilledButton(onPressed: () {}, child: Text('Login')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordTextField extends StatefulWidget {
+  final Function(String) onChanged;
+  const PasswordTextField({super.key, required this.onChanged});
+
+  @override
+  State<PasswordTextField> createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  bool isObscure = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: Row(
+        children: [
+          SizedBox(width: 48, height: 48),
+          Expanded(
+            child: TextField(
+              onChanged: widget.onChanged,
+              obscureText: isObscure,
+              textInputAction: TextInputAction.done,
+              decoration: InputDecoration(hintText: 'Password'),
+            ),
+          ),
+          IconButton(
+            onPressed: () => setState(() => isObscure = !isObscure),
+            icon: Icon(isObscure ? Icons.visibility : Icons.visibility_off),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/theme.dart
+++ lib/presentation/theme.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
-  final ColorScheme colorScheme = .fromSeed(seedColor: Colors.blue);
+  final Brightness brightness = .light;
+  final ColorScheme colorScheme = .fromSeed(seedColor: Colors.blue, brightness: brightness);
 
-  return ThemeData(colorScheme: colorScheme);
+  return ThemeData(
+    colorScheme: colorScheme,
+    brightness: brightness,
+
+    filledButtonTheme: FilledButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: WidgetStateColor.resolveWith((states) {
+          if (states.contains(WidgetState.pressed)) return colorScheme.inversePrimary;
+          if (states.contains(WidgetState.disabled)) return colorScheme.primaryContainer;
+          return colorScheme.primary;
+        }),
+        overlayColor: WidgetStatePropertyAll(Colors.transparent),
+        elevation: WidgetStatePropertyAll(0),
+        animationDuration: Duration.zero,
+        shape: WidgetStatePropertyAll(
+          RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.all(Radius.circular(3))),
+        ),
+      ),
+    ),
+
+    inputDecorationTheme: InputDecorationThemeData(
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(width: 2, color: colorScheme.primary),
+      ),
+      contentPadding: EdgeInsets.only(left: 5, top: 10, bottom: 10),
+      isDense: true,
+      hintStyle: TextStyle(fontWeight: FontWeight.w200, color: colorScheme.onSurfaceVariant),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(3),
+        borderSide: BorderSide(color: colorScheme.primary, width: 3),
+      ),
+    ),
+
+    iconButtonTheme: IconButtonThemeData(
+      style: ButtonStyle(
+        iconColor: WidgetStateProperty.resolveWith((states) {
+          if (states.contains(WidgetState.pressed)) return colorScheme.inversePrimary;
+          if (states.contains(WidgetState.disabled)) return colorScheme.primaryFixed;
+          return colorScheme.primary;
+        }),
+        overlayColor: WidgetStatePropertyAll(Colors.transparent),
+        elevation: WidgetStatePropertyAll(0),
+        animationDuration: Duration.zero,
+      ),
+    ),
+  );
 }
