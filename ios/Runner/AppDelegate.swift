bf83e641c30f1b6c24f7fb18b31fad4998779477
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2026-02-08 12:05:10 +0300

    Add screenshot warning to ios app

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,6 +10,23 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+      // 1. Listen for Screen Recording / Mirroring changes
+              NotificationCenter.default.addObserver(
+                  self,
+                  selector: #selector(handleScreenCaptureChange),
+                  name: UIScreen.capturedDidChangeNotification,
+                  object: nil
+              )
+              
+              // 2. Listen for Screenshots (Warning only - iOS blocks prevention)
+              NotificationCenter.default.addObserver(
+                  self,
+                  selector: #selector(handleScreenshotTaken),
+                  name: UIApplication.userDidTakeScreenshotNotification,
+                  object: nil
+              )
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
     
@@ -36,5 +53,48 @@ import UIKit
             privacyBlurView = nil
             
             super.applicationDidBecomeActive(application)
+        }
+    
+    // --- B. Handle Screen Recording ---
+        @objc func handleScreenCaptureChange() {
+            if UIScreen.main.isCaptured {
+                // User started recording -> Blur immediately
+                enablePrivacyScreen()
+            } else {
+                // User stopped recording -> Unblur
+                disablePrivacyScreen()
+            }
+        }
+        
+        // --- C. Handle Screenshot (After the fact) ---
+        @objc func handleScreenshotTaken() {
+            // We can't stop the screenshot, but we can annoy the user or log it.
+            let alert = UIAlertController(
+                title: "Screenshot Detected",
+                message: "For your security, please avoid taking screenshots of your private notes.",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            
+            self.window?.rootViewController?.present(alert, animated: true)
+        }
+
+        // --- Helper Functions ---
+        func enablePrivacyScreen() {
+            if privacyBlurView == nil {
+                let blurEffect = UIBlurEffect(style: .systemUltraThinMaterialDark) // Darker blur for privacy
+                privacyBlurView = UIVisualEffectView(effect: blurEffect)
+                
+                if let window = self.window {
+                    privacyBlurView?.frame = window.bounds
+                    privacyBlurView?.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+                    window.addSubview(privacyBlurView!)
+                }
+            }
+        }
+
+        func disablePrivacyScreen() {
+            privacyBlurView?.removeFromSuperview()
+            privacyBlurView = nil
         }
 }
