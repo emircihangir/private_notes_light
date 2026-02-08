93102741b7750af7200a04c37527cb571d12c40b
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2026-02-08 11:58:12 +0300

    Add android app hide screen on preview feature

--- android/app/src/main/kotlin/com/example/private_notes_light/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/private_notes_light/MainActivity.kt
@@ -2,4 +2,12 @@ package com.example.private_notes_light
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity(){
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        // This single line hides the app content in "Recents"
+        // AND prevents screenshots (bonus security).
+        window.setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE)
+    }
+}
