07d6034fc680299a1870298001186f235b9aebe1
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2026-02-13 13:29:35 +0300

    Add FLAG_SECURE to android app

--- android/app/src/main/kotlin/com/example/private_notes_light/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/private_notes_light/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.example.private_notes_light
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import android.view.WindowManager
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Add this line to prevent screenshots
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+    }
+}
