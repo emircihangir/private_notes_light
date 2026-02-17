434e3b939f252366a9b402beb741c3230b8bb914
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2026-02-17 20:27:45 +0300

    Add import_controller_test

---
+++ test/features/backup/application/import_controller_test.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mockito/mockito.dart';
+import 'package:private_notes_light/features/authentication/domain/credentials_data.dart';
+import 'package:private_notes_light/features/backup/application/file_picker_running.dart';
+import 'package:private_notes_light/features/backup/application/file_picker_service.dart';
+import 'package:private_notes_light/features/backup/application/import_controller.dart';
+import 'package:private_notes_light/features/backup/data/backup_repository.dart';
+import 'package:private_notes_light/features/backup/domain/backup_data.dart';
+import 'package:private_notes_light/features/backup/domain/import_controller_state.dart';
+import 'package:private_notes_light/features/encryption/application/encryption_service.dart';
+import 'package:private_notes_light/features/encryption/application/master_key.dart';
+import 'package:private_notes_light/features/notes/data/note_repository.dart';
+import 'package:private_notes_light/features/notes/domain/note_dto.dart';
+import 'package:private_notes_light/features/settings/data/settings_repository.dart';
+import 'package:private_notes_light/features/settings/domain/settings_data.dart';
+import 'package:encrypt/encrypt.dart' as enc;
+
+@GenerateNiceMocks([
+  MockSpec<BackupRepository>(),
+  MockSpec<EncryptionService>(),
+  MockSpec<NoteRepository>(),
+  MockSpec<FilePickerService>(),
+  MockSpec<SettingsRepository>(),
+])
+import 'import_controller_test.mocks.dart';
+
+void main() {
+  late MockBackupRepository mockBackupRepo;
+  late MockEncryptionService mockEncryptionService;
+  late MockNoteRepository mockNoteRepo;
+  late MockFilePickerService mockFilePickerService;
+  late ProviderContainer container;
+
+  setUp(() {
+    mockBackupRepo = MockBackupRepository();
+    mockEncryptionService = MockEncryptionService();
+    mockNoteRepo = MockNoteRepository();
+    mockFilePickerService = MockFilePickerService();
+
+    container = ProviderContainer(
+      overrides: [
+        backupRepositoryProvider.overrideWith((_) => mockBackupRepo),
+        noteRepositoryProvider.overrideWithValue(mockNoteRepo),
+        encryptionServiceProvider.overrideWithValue(mockEncryptionService),
+        filePickerServiceProvider.overrideWith((_) => mockFilePickerService),
+      ],
+    );
+    addTearDown(container.dispose);
+  });
+
+  group('Import Controller Tests', () {});
+}
