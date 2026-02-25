94239f608ea986e1477504587fff757c55067df2
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2026-02-25 12:22:24 +0300

    Add test for ImportListTile

--- test/features/backup/presentation/import_list_tile_test.dart
+++ test/features/backup/presentation/import_list_tile_test.dart
@@ -4,10 +4,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:private_notes_light/features/backup/application/import_controller.dart';
 import 'package:private_notes_light/features/backup/domain/import_controller_state.dart';
 import 'package:private_notes_light/features/backup/presentation/import_list_tile.dart';
+import 'package:private_notes_light/features/backup/presentation/import_password_dialog.dart';
 import 'package:private_notes_light/features/backup/presentation/overwrite_warning_dialog.dart';
 import 'package:private_notes_light/features/notes/presentation/notes_page.dart';
 import 'package:private_notes_light/features/settings/presentation/settings_page.dart';
 import 'package:private_notes_light/l10n/app_localizations.dart';
+
+import '../../../core/dummy_backup_data.dart';
 
 void main() {
   testWidgets('can show overwrite warning.', (widgetTester) async {
@@ -88,5 +91,30 @@ void main() {
 
     // Verify
     expect(find.text(AppLocalizations.of(notesPageContext)!.importSuccess), findsOne);
+  });
+
+  testWidgets('can show password dialog.', (widgetTester) async {
+    // Setup
+    await widgetTester.pumpWidget(
+      ProviderScope(
+        child: MaterialApp(
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: SettingsPage(),
+        ),
+      ),
+    );
+    await widgetTester.pump();
+    final context = widgetTester.element(find.byType(ImportListTile));
+    final container = ProviderScope.containerOf(context);
+
+    // Act
+    container
+        .read(importControllerProvider.notifier)
+        .setState(ImportControllerState.showPasswordDialog(dummyBackupData()));
+    await widgetTester.pumpAndSettle();
+
+    // Verify
+    expect(find.byType(ImportPasswordDialog), findsOne);
   });
 }
