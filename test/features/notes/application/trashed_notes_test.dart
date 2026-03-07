a5a5971f4aeda89aa56a4d22efb34a1f5762fb64
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2026-03-07 21:46:35 +0000

    Add test for TrashedNotes.undoLast()

--- test/features/notes/application/trashed_notes_test.dart
+++ test/features/notes/application/trashed_notes_test.dart
@@ -6,7 +6,7 @@ import 'package:private_notes_light/features/notes/domain/trashed_note_data.dart
 
 void main() {
   group('TrashedNotes tests ->', () {
-    test('add works', () async {
+    test('add works', () {
       // Setup
       final container = ProviderContainer();
       addTearDown(container.dispose);
@@ -20,6 +20,27 @@ void main() {
       final after = container.read(trashedNotesProvider);
       expect(after.length - before.length, 1);
       expect(after.contains(dummyValue), isTrue);
+    });
+
+    test('undoLast works', () {
+      // Setup
+      final container = ProviderContainer();
+      addTearDown(container.dispose);
+
+      final dummyValue1 = TrashedNoteData(NoteWidgetData(noteId: '', noteTitle: ''), 0);
+      final dummyValue2 = TrashedNoteData(NoteWidgetData(noteId: '', noteTitle: ''), 1);
+      container.read(trashedNotesProvider.notifier).add(dummyValue1);
+      container.read(trashedNotesProvider.notifier).add(dummyValue2);
+
+      // Act
+      final retrievedValue = container.read(trashedNotesProvider.notifier).undoLast();
+
+      // Verify
+      final after = container.read(trashedNotesProvider);
+      expect(retrievedValue, dummyValue2);
+      expect(after.length, 1);
+      expect(after.contains(dummyValue1), isTrue);
+      expect(after.contains(dummyValue2), isFalse);
     });
   });
 }
