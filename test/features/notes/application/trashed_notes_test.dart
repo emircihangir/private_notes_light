e0f8f23fe704bcde9755bf831459e5eaea887ea7
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2026-03-07 21:55:43 +0000

    Add test for TrashedNotes.emptyTrash()

--- test/features/notes/application/trashed_notes_test.dart
+++ test/features/notes/application/trashed_notes_test.dart
@@ -1,10 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:private_notes_light/features/notes/application/trashed_notes.dart';
+import 'package:private_notes_light/features/notes/data/note_repository.dart';
 import 'package:private_notes_light/features/notes/domain/note_widget_data.dart';
 import 'package:private_notes_light/features/notes/domain/trashed_note_data.dart';
 
+@GenerateNiceMocks([MockSpec<NoteRepository>()])
+import 'trashed_notes_test.mocks.dart';
+
 void main() {
+  late MockNoteRepository mockNoteRepo;
+
+  setUp(() {
+    mockNoteRepo = MockNoteRepository();
+  });
+
   group('TrashedNotes tests ->', () {
     test('add works', () {
       // Setup
@@ -41,6 +53,27 @@ void main() {
       expect(after.length, 1);
       expect(after.contains(dummyValue1), isTrue);
       expect(after.contains(dummyValue2), isFalse);
+    });
+
+    test('emptyTrash works', () async {
+      // Setup
+      final container = ProviderContainer(
+        overrides: [noteRepositoryProvider.overrideWith((ref) => mockNoteRepo)],
+      );
+      addTearDown(container.dispose);
+
+      final dummyValue1 = TrashedNoteData(NoteWidgetData(noteId: '', noteTitle: ''), 0);
+      final dummyValue2 = TrashedNoteData(NoteWidgetData(noteId: '', noteTitle: ''), 1);
+      container.read(trashedNotesProvider.notifier).add(dummyValue1);
+      container.read(trashedNotesProvider.notifier).add(dummyValue2);
+
+      // Act
+      await container.read(trashedNotesProvider.notifier).emptyTrash();
+
+      // Verify
+      final after = container.read(trashedNotesProvider);
+      expect(after.length, 0);
+      verify(mockNoteRepo.batchDelete(argThat(isA<List<String>>()))).called(1);
     });
   });
 }
