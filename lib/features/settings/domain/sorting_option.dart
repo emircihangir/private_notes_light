7222d3fe877c2677144b8357d01100e1abecc283
Muhammed Emir Cihangir <m.emircihangir@gmail.com>
2026-04-17 18:38:29 +0100

    Change the sortingValue for alphabetical sorting

--- lib/features/settings/domain/sorting_option.dart
+++ lib/features/settings/domain/sorting_option.dart
@@ -1,8 +1,8 @@
 enum SortingOption {
   newestFirst('dateCreated DESC'),
   oldestFirst('dateCreated ASC'),
-  aToZ('title ASC'),
-  zToA('title DESC');
+  aToZ('title COLLATE NOCASE ASC'),
+  zToA('title COLLATE NOCASE DESC');
 
   final String sortingValue;
   const SortingOption(this.sortingValue);
